3a60c9c10532e04407989f47a6366d2ade7a7286
Archie9010 <archie9010@protonmail.com>
2022-12-29 09:41:53 +0000

    corrected allowed host

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,7 +60,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://drf-api9010.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'drf-api9010.herokuapp.com']
 
 # Application definition
 
